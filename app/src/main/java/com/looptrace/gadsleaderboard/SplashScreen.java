1ea30ca4906ca4914a8c152f60b8143bf7870ed0
Onwe Onweokoh-Nwaoji Chukwumaobim <kr8tiveonwe@gmail.com>
2020-08-30 16:08:28 +0100

    SplashScreen and Toolbar Setup Completed

--- app/src/main/java/com/looptrace/gadsleaderboard/SplashScreen.java
+++ app/src/main/java/com/looptrace/gadsleaderboard/SplashScreen.java
@@ -1,17 +1,15 @@
 package com.looptrace.gadsleaderboard;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar mToolbar = findViewById(R.id.mToolbar);
-        setSupportActionBar(mToolbar);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
